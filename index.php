071e38beb20f4cbe016b4bb6126b5284e0d25b7f
MTRNord <mtrnord1@gmail.com>
2015-01-28 10:16:25 +0100

    next try

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <script>
-  if (window.File && window.FileReader && window.FileList && window.Blob) {
-    // Great success! All the File APIs are supported.
-  } else {
-    alert('The File APIs are not fully supported in this browser.');
-  }
-  var reader = new FileReader();
-
-  var fileContents = reader.readAsText('../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/toolbar.html');
+  var fileContents = System.IO.File.ReadAllLines('../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/toolbar.html');
   $('#editor').closest( "div" ).innerHTML(fileContents);
 </script>
 <script>
