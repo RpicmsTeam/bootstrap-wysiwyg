3264b552d4bc08e094a2bed649d4ed7509fde648
MTRNord <mtrnord1@gmail.com>
2015-01-28 09:46:11 +0100

    fix path

--- html_header.php
+++ html_header.php
@@ -1,3 +1,3 @@
 <script src="../../core/libs/theme_engine/jquery/plugins/jquery.hotkeys.js"></script>
-<link href="index.css" rel="stylesheet">
-<script src="bootstrap-wysiwyg.js"></script>
+<link href="../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/index.css" rel="stylesheet">
+<script src="../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/bootstrap-wysiwyg.js"></script>
