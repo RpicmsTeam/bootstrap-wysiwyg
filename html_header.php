66f6912c2d2be1e23be1e3e3e1c3a31d3b939a17
MTRNord <mtrnord1@gmail.com>
2015-01-28 10:55:23 +0100

    add missing lib

--- html_header.php
+++ html_header.php
@@ -1,3 +1,4 @@
 <script src="../../core/libs/theme_engine/jquery/plugins/jquery.hotkeys.js"></script>
 <link href="../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/index.css" rel="stylesheet">
 <script src="../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/bootstrap-wysiwyg.js"></script>
+<script src="../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/external/google-code-prettify/prettify.js"></script>
