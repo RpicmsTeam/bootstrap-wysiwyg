80a50a29d50c679e0ad6343cea9e8614fc8e3f6a
MTRNord <mtrnord1@gmail.com>
2015-01-28 10:59:56 +0100

    chrome....

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script>
   $('#editor').closest('div').load("../../core/backend/admin/modules/modul_simple_bootstrap-wysiwyg/toolbar.html");
   //$('#editor').closest('div').innerHTML(file);
