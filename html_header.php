82fb4945a47bb4542ac91dcded8d013d187a2e37
MTRNord <mtrnord1@gmail.com>
2015-01-28 09:29:00 +0100

    update path

--- html_header.php
+++ html_header.php
@@ -1,4 +1,3 @@
-    <script src="external/jquery.hotkeys.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-    <link href="index.css" rel="stylesheet">
-    <script src="bootstrap-wysiwyg.js"></script>
+<script src="../../core/libs/theme_engine/jquery/plugins/jquery.hotkeys.js"></script>
+<link href="index.css" rel="stylesheet">
+<script src="bootstrap-wysiwyg.js"></script>
